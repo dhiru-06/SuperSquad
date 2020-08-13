19fdcae4b0e6516e567c268d84275849d2e086a9
dhiru-06 <dhiru199706@gmail.com>
2020-08-13 20:12:06 +0530

    Updates

---
+++ precache-manifest.c102723c3f455ff800521499bb51b226.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1ef986c6a8e391da87644514054cba",
+    "url": "/SuperSquad/index.html"
+  },
+  {
+    "revision": "ac01b1688ec9d5b96448",
+    "url": "/SuperSquad/static/css/main.49fb76cc.chunk.css"
+  },
+  {
+    "revision": "deaa9601150df1841224",
+    "url": "/SuperSquad/static/js/2.75b17cce.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/SuperSquad/static/js/2.75b17cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac01b1688ec9d5b96448",
+    "url": "/SuperSquad/static/js/main.bafcd27b.chunk.js"
+  },
+  {
+    "revision": "18217f12d9181056f0c6",
+    "url": "/SuperSquad/static/js/runtime-main.ab62cc3f.js"
+  },
+  {
+    "revision": "117150ca7678bcd8897c7f8bfcf60347",
+    "url": "/SuperSquad/static/media/bg.117150ca.jpg"
+  }
+]);
